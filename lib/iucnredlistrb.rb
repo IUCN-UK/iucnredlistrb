23ebcaa6de726d433ef0440b0fdb44f2b6757beb
Ian Townsend <ian.townsend@gmail.com>
2025-03-24 09:51:50 +0000

    Initial commit 🌸

---
+++ lib/iucnredlistrb.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "iucnredlistrb/version"
+
+module Iucnredlistrb
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/test_iucnredlistrb.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class TestIucnredlistrb < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::Iucnredlistrb::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
