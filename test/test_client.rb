db6d74cb1fe0ebb396f1b24ce0beb77f776ad851
Ian Townsend <ian.townsend@gmail.com>
2025-03-24 11:25:00 +0000

    Move comment

--- test/test_client.rb
+++ test/test_client.rb
@@ -6,18 +6,6 @@ require "json"
 # require "webmock/minitest"
 
 module IUCNRedListRb
-  # IUCNRedListRb::Client
-  #
-  # This class serves as the main entry point for interacting with the IUCN Red List API.  It delegates to
-  # the IUCNRedListRb classes to provide access to the various endpoints available in the Red List API.
-  #
-  # For detailed API documentation, visit:
-  # https://api.iucnredlist.org/api-docs/index.html
-  #
-  # Example usage:
-  #
-  #   client = IUCNRedListRb::Client.new
-  #
   class ClientTest < Minitest::Test
     def setup
       @client = Client.new
